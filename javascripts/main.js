11e8a6abbc0eb1862f0ea05d39c5ec817b4c1574
OlgaM09 <odmironova@edu.hse.ru>
2025-01-28 22:51:04 +0100

    update

---
+++ javascripts/main.js
@@ -0,0 +1,3 @@
+console.log('cheers');
+
+// document.getElementById("text").addEventListener("click", function() {alert("I GOT CLICKED"); });
--- js.js
+++
@@ -1,4 +0,0 @@
-console.log('cheers');
-
-document.getElementById("text").addEventListener("click", function() {
-    alert("I GOT CLICKED");    });
